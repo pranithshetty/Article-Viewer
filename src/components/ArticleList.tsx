aee6849da8395cdf5fc145667757c237bf28d49d
pranithshetty <shetty.praneeth4@gmail.com>
2024-02-25 22:08:52 +0530

    minor style change

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -19,10 +19,10 @@ const ArticleList = () => {
             {!loading && !error && (
                 <div className='h-full'>
                     <h1 className='font-bold text-3xl mb-2 text-slate-300'>Articles</h1>
-                    <div className='h-fit grid grid-cols-4 gap-1'>
+                    <div className='h-fit grid grid-cols-1 gap-1 sm:grid-cols-4 md:max-xl:grid-cols-2'>
                         {articles.map((article: any) => (
-                            <Link to={`/article/${article.id}`}>
-                                <div className='rounded-lg border h-[250px] border-yellow-300/20 overflow-hidden shadow-lg text-left mb-2 p-5 m-5 bg-slate-200/20 bg-opacity-50 text-slate-300' key={article.id}>
+                            <Link to={`/article/${article.id}`} key={article.id}>
+                                <div className='rounded-lg border h-[250px] border-yellow-300/20 overflow-hidden shadow-lg text-left mb-2 p-5 m-5 bg-slate-200/20 bg-opacity-50 text-slate-300'>
                                     <h1 className='font-semibold text-yellow-300'>{article.title}</h1>
                                     <p className='text-sm pt-4 font-mono line-clamp-5 overflow-hidden'>{article.summary}</p>
                                 </div>
