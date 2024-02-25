b3b7c393f4d763a1752edb1d57ba8320e62fad1b
pranithshetty <shetty.praneeth4@gmail.com>
2024-02-25 18:38:24 +0530

    Removed unneceary test files

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Article Viewer
 
 An application that displays articles.
+
+## Tech Stack
+
+**Client:** React, Redux-Toolkit, TailwindCSS,Axios, React-Router
+
+## Features
+
+- View Article list
+- View Indivisual articles
--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArticleList from './components/ArticleList';
 import Article from './components/Article';
-import Test1 from './components/Test1';
-import Test2 from './components/Test2';
 import { Provider } from 'react-redux';
 import { store } from './store/store'
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/" element={<Test1 />} />
-          <Route path="/article/:id" element={<Test2 />} />
+          <Route path="/" element={<ArticleList />} />
+          <Route path="/article/:id" element={<Article />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </Provider>
